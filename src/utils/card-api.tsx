f888794bb0d0db98e953a0063342c14c01d92431
Romanovskiy Ivan <vip1103@yandex.ru>
2024-08-25 00:55:13 +0300

    feat: added description gif and changed api

--- src/utils/card-api.tsx
+++ src/utils/card-api.tsx
@@ -1,13 +1,15 @@
-import { UnsplashPhoto } from '../services/cardsSlice';
+import { UnsplashPhoto } from "../services/cardsSlice";
 
 export const getCardsApi = async (): Promise<UnsplashPhoto[]> => {
-	const response = await fetch("https://api.unsplash.com/photos/random?count=4", {
-		headers: {
-			Authorization: `Client-ID ${process.env.REACT_APP_UNSPLASH_CLIENT_ID}`,
-		},
-	});
-	
+  const response = await fetch(
+    "https://api.unsplash.com/photos/random?count=4",
+    {
+      headers: {
+        Authorization: `Client-ID 0ZK6sRo5QB1mwJA35gwJQvlsLJzq1eN-SWbUYkKkgSI`,
+      },
+    }
+  );
+
   const data: UnsplashPhoto[] = await response.json();
   return data;
 };
-
